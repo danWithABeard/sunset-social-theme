eff54af196651cb0153c5e5d9415bcdd792d3da8
Dan Cicconi <dcicconi@paperlesspost.com>
2019-06-11 10:04:10 -0400

    Add mailing list signup to footer

--- footer.php
+++ footer.php
@@ -16,6 +16,32 @@
       </div>
       
       <div class="w-container">
+        <!-- Begin Mailchimp Signup Form -->
+        <div id="mc_embed_signup">
+          <h3>Subscribe to our mailing list</h3>
+          <form action="https://sunsetsocialphl.us20.list-manage.com/subscribe/post?u=d8c5649765efbe7df0f48d1a0&amp;id=f936fa6d44" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <div id="mc_embed_signup_scroll">
+              <div class="mc-field-group">
+                <input type="email" value="" name="EMAIL" class="required email text-field w-input" id="mce-EMAIL" placeholder="donna@sunsetsocialphl.com"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="submit-button w-button">
+              </div>
+                
+              <div id="mce-responses" class="clear">
+                <div class="response" id="mce-error-response" style="display:none"></div>
+                <div class="response" id="mce-success-response" style="display:none"></div>
+              </div>    
+
+              <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->      
+              <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_d8c5649765efbe7df0f48d1a0_f936fa6d44" tabindex="-1" value=""></div>
+            </div>
+          </form>
+        </div>
+        
+        <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
+        <script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+        <!--End mc_embed_signup-->
+      </div>
+
+      <div class="w-container">
         <div class="div-block">
           
           <div>
@@ -45,7 +71,6 @@
         </div>
       </div>
     </div>
-
     <div class="order-button__outer">
       <a href="https://www.toasttab.com/sunset-social/v2/online-order#!/" title="Order online from Sunset Social" class="order-button__inner">ORDER NOW!</a>
     </div>
